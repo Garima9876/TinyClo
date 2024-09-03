cdbbbdfb6a1db83a93a4f427b0e1e3ccf03f21ad
Garima9876 <garimabhatt987@gmail.com>
2024-09-03 10:00:07 +0530

    created all the required components for landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import Categories from './components/Categories';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Categories />
+      <Footer />
     </div>
   );
 }
