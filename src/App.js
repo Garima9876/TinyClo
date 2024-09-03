0b635191531294f4e5a9e3610370ce004bca0a88
Garima9876 <garimabhatt987@gmail.com>
2024-09-03 12:16:19 +0530

    completed the navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Categories from './components/Categories';
 import Footer from './components/Footer';
-import './App.css';
+import './components/styles/custom-bootstrap.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <Categories />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div></div>;
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="TinyClo Logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            {[
+              "Home",
+              "Why Rent?",
+              "Clean Out Closet",
+              "About Us",
+              "Store",
+              "Contact Us",
+            ].map((item) => (
+              <li className="nav-item" key={item}>
+                <Link to="/" className="nav-link">
+                  <button
+                    className={`btn ${
+                      activeButton === item
+                        ? "btn-secondary rounded-pill"
+                        : "btn"
+                    }`}
+                    onClick={() => handleClick(item)}
+                  >
+                    {item}
+                  </button>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <Link to="/login" className="ms-auto">
+            <button className="btn btn-secondary ms-3">Login/Sign Up</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
